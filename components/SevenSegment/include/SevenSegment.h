97e8be628e72a37b2522454e4cadd363befdda4d
Nitiphum <65030130@kmitl.ac.th>
2024-08-18 22:08:08 +0700

    commit

--- components/SevenSegment/include/SevenSegment.h
+++ components/SevenSegment/include/SevenSegment.h
@@ -1,6 +1,7 @@
 #include "LED.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+ 
 class SevenSegment
 {
     int common_pin;
@@ -11,13 +12,22 @@ class SevenSegment
     LED seg_e = LED(19);
     LED seg_f = LED(21);
     LED seg_g = LED(22);
-    LED common = LED(common_pin);
+    LED common;
+ 
 public:
-    SevenSegment(int pin)
-    {
-        common_pin = pin;
-        common = LED(common_pin);
-    } 
+    SevenSegment(int pin) : common_pin(pin), common(LED(pin)) {}
+ 
     void HardwareTest();
     void DisplayBlank();
+    void DisplayNumber(int number); // Updated to take an int parameter
+    void DisplayNum0();
+    void DisplayNum1();
+    void DisplayNum2();
+    void DisplayNum3();
+    void DisplayNum4();
+    void DisplayNum5();
+    void DisplayNum6();
+    void DisplayNum7();
+    void DisplayNum8();
+    void DisplayNum9();
 };
